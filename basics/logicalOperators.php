929a912292e124cbdca2240121bd4afd88fdadf3
SinaShaham <sinashaham@gmail.com>
2024-05-11 13:48:06 -0700

    logical operators updated

---
+++ basics/logicalOperators.php
@@ -0,0 +1,27 @@
+<?php
+// http://localhost/Project1/PHP-Learning-Hub/basics/logicalOperators.php
+$x = 10;
+
+$y = '10';
+
+
+// two equal signs only evaluates value and not the type
+var_dump($x == $y);
+
+
+// ===
+var_dump($x === $y);
+
+
+//!=
+var_dump($x !== $y);
+
+
+// Logical operators
+
+
+$a = 10;
+$b = 20;
+
+var_dump($a==11 && $b == 20);
+?>
